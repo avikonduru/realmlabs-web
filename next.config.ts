97a614c79312c9b7aa78ee283988360b9347c894
avikonduru <avikonduru@gmail.com>
2025-01-05 08:21:20 -0800

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	experimental: {
-		optimizePackageImports: ['@chakra-ui/react'],
-	},
+  output: 'export', // Ensures `next export` is compatible
+  trailingSlash: true, // Optional: ensures consistent paths
+  images: {
+    unoptimized: true, // Required if using the Next.js Image component
+  },
+  experimental: {
+    optimizePackageImports: ['@chakra-ui/react'],
+  },
 };
 
 export default nextConfig;
