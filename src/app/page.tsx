841836f95e0cbb96ef07589143f76d3080024ff1
avikonduru <avikonduru@gmail.com>
2025-01-05 07:49:03 -0800

    adding subscription settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
 
 import { Button } from '@/components/ui/button';
 import { Switch } from '@/components/ui/switch';
-import { toaster } from '@/components/ui/toaster';
+import { toaster, Toaster } from '@/components/ui/toaster';
+import { Skeleton, SkeletonCircle, SkeletonText } from '@/components/ui/skeleton';
 import {
   Container,
   Card,
@@ -18,59 +20,121 @@ import {
 } from '@chakra-ui/react';
 
 const HomwPage = () => {
+  const searchParams = useSearchParams();
+
+  const [loading, setLoading] = useState(true);
+  const [subscriberSettings, setSubscriberSettings] = useState({
+    unsubscribeToMarketingEmails: false,
+    unsubscribeToAllEmails: false,
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userId = searchParams.get('userId') || '';
+
+        console.log({ userId });
+
+        await toaster.promise(
+          new Promise((resolve) => {
+            setTimeout(() => {
+              setLoading(false);
+              resolve(true);
+              setSubscriberSettings({
+                ...subscriberSettings,
+                unsubscribeToMarketingEmails: false,
+                unsubscribeToAllEmails: true,
+              });
+            }, 5000);
+          }),
+          {
+            loading: {
+              title: 'Loading your subscription status...',
+              description: 'Please wait...',
+            },
+            success: {
+              title: 'Update successful',
+              description: 'Your subscription status has been updated.',
+              action: {
+                label: 'Undo',
+                onClick: () => {
+                  setSubscriberSettings({
+                    ...subscriberSettings,
+                    unsubscribeToAllEmails: false,
+                  });
+                },
+              },
+            },
+            error: {
+              title: 'Error',
+              description: 'Failed to load subscription status',
+            },
+          }
+        );
+      } catch (error) {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Container maxW="lg">
+      <Toaster />
       <Flex mt="8" h="100%" align="center" justify="center">
         <Card.Root variant="subtle" width="full">
-          <Card.Body>
-            <Box mb="8">
-              <Card.Title>Hi avikonduru@gmail.com,</Card.Title>
-              <Card.Description fontSize="sm" color="muted">
-                Manage your subscriptions
-              </Card.Description>
-            </Box>
+          {loading ? (
+            <Card.Body>
+              <Box mb="8">
+                <SkeletonText noOfLines={2} />
+              </Box>
+              <Skeleton height="200px" />
+            </Card.Body>
+          ) : (
+            <Card.Body>
+              <Box mb="8">
+                <Card.Title>Hi avikonduru@gmail.com,</Card.Title>
+                <Card.Description fontSize="sm" color="muted">
+                  Manage your subscriptions
+                </Card.Description>
+              </Box>
 
-            <Stack separator={<StackSeparator />} gap="4">
-              <Flex align="center" justify="space-between">
-                <Text fontSize="sm" fontWeight="semibold" color="muted">
-                  Monthly
-                </Text>
-                <Switch size="lg" />
-              </Flex>
+              <Stack separator={<StackSeparator />} gap="4">
+                <Flex align="center" justify="space-between">
+                  <Text fontSize="sm" fontWeight="semibold" color="muted">
+                    Unsubscribe to marketing emails
+                  </Text>
+                  <Switch
+                    size="lg"
+                    checked={subscriberSettings.unsubscribeToMarketingEmails}
+                    onCheckedChange={({ checked }) => {
+                      setSubscriberSettings({
+                        ...subscriberSettings,
+                        unsubscribeToMarketingEmails: checked,
+                      });
+                    }}
+                  />
+                </Flex>
 
-              <Flex align="center" justify="space-between">
-                <Text fontSize="sm" fontWeight="semibold" color="muted">
-                  Monthly
-                </Text>
-                <Switch size="lg" />
-              </Flex>
-
-              <Flex align="center" justify="space-between">
-                <Text fontSize="sm" fontWeight="semibold" color="muted">
-                  Monthly
-                </Text>
-                <Switch size="lg" />
-              </Flex>
-            </Stack>
-
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={() => {
-                console.log('clicked');
-                toaster.success({
-                  title: 'Update successful',
-                  description: 'File saved successfully to the server',
-                  action: {
-                    label: 'Undo',
-                    onClick: () => console.log('Undo'),
-                  },
-                });
-              }}
-            >
-              Click me
-            </Button>
-          </Card.Body>
+                <Flex align="center" justify="space-between">
+                  <Text fontSize="sm" fontWeight="semibold" color="muted">
+                    Unsubscribe to all emails
+                  </Text>
+                  <Switch
+                    size="lg"
+                    checked={subscriberSettings.unsubscribeToAllEmails}
+                    onChange={(e) =>
+                      setSubscriberSettings({
+                        ...subscriberSettings,
+                        unsubscribeToAllEmails: e.target.checked,
+                      })
+                    }
+                  />
+                </Flex>
+              </Stack>
+            </Card.Body>
+          )}
 
           <Card.Footer>
             <Flex align="center" justify="center" gap="1" width="full">
@@ -78,7 +142,7 @@ const HomwPage = () => {
                 Powered By:
               </Text>
               <Text fontSize="xs" fontWeight="bold">
-                <Link variant="underline" href="https://chakra-ui.com" colorPalette="blue">
+                <Link variant="underline" href="https://www.realmlabs.co/" colorPalette="blue">
                   Realm Labs
                 </Link>
               </Text>
